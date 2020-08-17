abf29e68305a5e7ff61c43dd8c0be0a7aad9ba8c
Alexis <alkryzhanov@hotmail.com>
2020-08-17 17:55:56 +0300

    Updates

---
+++ precache-manifest.416824865d6089d307bd3628ab11f5e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "657caaae40b21cf0bbc28b17eddce210",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "41bea441ef1a4d5b11c8",
+    "url": "/react-todo/static/css/main.41beef0b.chunk.css"
+  },
+  {
+    "revision": "e9b4b309e56915c53fb9",
+    "url": "/react-todo/static/js/2.b5e4b9ab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo/static/js/2.b5e4b9ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41bea441ef1a4d5b11c8",
+    "url": "/react-todo/static/js/main.dbcf4526.chunk.js"
+  },
+  {
+    "revision": "cfa8152b93379cf365f9",
+    "url": "/react-todo/static/js/runtime-main.eac2ca12.js"
+  }
+]);
